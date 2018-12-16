6160be820abbeada826e0ac9ac106f125d6494d3
abarabone <abara00@live.jp>
2018-12-16 18:54:24 +0900

    create

---
+++ Assets/NeuronUnit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeuronUnit : MonoBehaviour
+{
+
+	public float	limit;
+
+	(NeuronUnit n, float ratio)[]	forwardLinks;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var tf	= this.transform;
+		tf.position = Random.insideUnitSphere * 100.0f;
+
+		void setPosition()
+		{
+
+		}
+		void setLinks()
+		{
+
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
