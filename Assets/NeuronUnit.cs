3bb0931d58df4ec444e7360e81e72114b5467e2e
abarabone <abara00@live.jp>
2018-12-17 19:09:57 +0900

    aaa

--- Assets/NeuronUnit.cs
+++ Assets/NeuronUnit.cs
@@ -6,47 +6,77 @@ using UnityEngine;
 public class NeuronUnit : MonoBehaviour
 {
 
-	public float	limit;
+	float	volume;
 
+	[SerializeField]
 	(NeuronUnit n, float v)[]	forwardLinks;
 
+	Zone zone;
 
-	public Zone	selfZone;
-	public Zone	outZone;
-	
+	Vector3	forward;// 軸索の方向、あとで入れるかも（それぞれが一番近い出力ゾーンに向かう、でいいかな？遠い場合は平均？）
 
 
-	public void Spark( float v )
+	MaterialPropertyBlock	mpb;
+	static readonly int		colorNameId = Shader.PropertyToID( "Color_" );
+
+
+	public void Emit( float v )
 	{
-		if( v >)
 
-		foreach( var node in forwardLinks )
+		if( !isEmit() ) return;
+		
+		forwardPropagation();
+		showState();
+
+		return;
+
+
+		bool isEmit()
 		{
-			if( node.v )
+			this.volume += v;
+
+			return this.volume >= 1.0f;
+		}
+		void forwardPropagation()
+		{
+			foreach( var node in this.forwardLinks )
+			{
+				node.n.Emit( node.v );
+			}
+		}
+		void showState()
+		{
+			var vol = this.volume;
+			this.mpb.SetColor( NeuronUnit.colorNameId, new Color(vol,vol,vol) );
 		}
 	}
-	public void 
 
-	
-    void Start()
-    {
+	private void Awake()
+	{
+
         var tf	= this.transform;
 
-		setPosition();
+		this.zone	= this.GetComponentInParent<Zone>();
+		this.mpb	= new MaterialPropertyBlock();
+		this.GetComponent<MeshRenderer>().SetPropertyBlock( this.mpb );
+		
+	}
+
+	void Start()
+    {
+
+        var tf	= this.transform;
+
 		setLinks();
 		
 		return;
 
-		void setPosition()
-		{
-			tf.position = selfZone.Tf.position + Random.insideUnitSphere * selfZone.Radius;
-		}
 
 		void setLinks()
 		{
-			this.forwardLinks = Physics
-				.OverlapSphere( tf.position, selfZone.linkRadius )
-				.Select( x => (x.GetComponent<NeuronUnit>(), 0.0f) )
+			this.forwardLinks = Physics.OverlapSphere( tf.position, this.zone.linkRadius )
+				.Select( x => (n:x.GetComponent<NeuronUnit>(), v:0.0f) )
+				.Select( x => (x.n, Random.value))// v のランダム初期化
 				.ToArray()
 				;
 		}
--- Assets/Zone.cs
+++ Assets/Zone.cs
@@ -1,19 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Zone : MonoBehaviour
 {
 
-	public Transform	Tf { get; private set; }
+	//public Transform	Tf { get; private set; }
 	public float		Radius;
 
 	public float	linkRadius;
+	public int		MaxNeurons;
+	
+	public Zone[]	forwardZones;
 
+
+	public NeuronUnit	NeuronTemplate;
+
+
+	Collider	Shape;
+	
+	public Vector3 GetRandomPosition( int i )
+	{
+		while( false )
+		{
+			Random.InitState( i );
+			var bbox	= this.Shape.bounds;
+			var px		= Random.Range( bbox.min.x,bbox.max.x );
+			var py		= Random.Range( bbox.min.y,bbox.max.y );
+			var pz		= Random.Range( bbox.min.z,bbox.max.z );
+			var rndInBbox = new Vector3( px, py, pz );
+
+			if( this.Shape.ClosestPoint(rndInBbox) != rndInBbox ) return rndInBbox;
+		}return new Vector3();
+	}
 
 	private void Awake()
 	{
-		this.Tf	= this.transform;
+		this.Shape	= this.GetComponent<Collider>();
+
+		var tf	= this.transform;
+
+		createNeurons();
+
+		return;
+
+
+		void createNeurons()
+		{
+			foreach( var i in Enumerable.Range(0, this.MaxNeurons) )
+			{
+				GameObject.Instantiate<NeuronUnit>
+					( this.NeuronTemplate, this.GetRandomPosition(i), Quaternion.identity, tf );
+			}
+		}
 	}
 
 }
