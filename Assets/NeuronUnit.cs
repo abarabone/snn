54a41fbcf51c774822a42b75cd666333f102a253
abarabone <abara00@live.jp>
2018-12-22 22:45:57 +0900

    aaa

--- Assets/NeuronUnit.cs
+++ Assets/NeuronUnit.cs
@@ -6,27 +6,33 @@ using UnityEngine;
 public class NeuronUnit : MonoBehaviour
 {
 
+	[SerializeField]
 	float	volume;
 
+	//(NeuronUnit n, float v)[]	forwardLinks;
 	[SerializeField]
-	(NeuronUnit n, float v)[]	forwardLinks;
+	LinkUnit[]	forwardLinks;
+	[SerializeField]
+	List<LinkUnit>	backLinks;
 
 	[HideInInspector]
 	public ZoneBase	ParentZone;
 
-	public float	UnitLinkRadius;
-	public float	UnitLinkArmDistance;
+	//public float	UnitLinkRadius;
+	//public float	UnitLinkArmDistance;
 
 	Vector3	linkerCenter;
 	Vector3	position;
 
 
+	Renderer				rndrr;
 	MaterialPropertyBlock	mpb;
-	static readonly int		colorNameId = Shader.PropertyToID( "Color_" );
+	static readonly int		colorNameId = Shader.PropertyToID( "_Color" );
 
 
 	public void Emit( float v )
 	{
+		addVolume();
 
 		if( !isEmit() ) return;
 		
@@ -35,37 +41,52 @@ public class NeuronUnit : MonoBehaviour
 		return;
 
 
-		bool isEmit()
+		void addVolume()
 		{
 			this.volume += v;
-
-			return this.volume >= 1.0f;
+		}
+		bool isEmit()
+		{
+			return this.volume >= this.backLinks.Count * 0.3f;
 		}
 		void forwardPropagation()
 		{
-			foreach( var node in this.forwardLinks )
+			foreach( var link in this.forwardLinks )
 			{
-				node.n.Emit( node.v );
+				link.end.Emit( link.value );
 			}
 		}
 	}
 
-	private void Awake()
+	public void Effect( float v )
+	{
+
+	}
+
+	public void Clear()
+	{
+		this.volume = 0.0f;
+	}
+
+
+	protected void Awake()
 	{
 		initValues();
 
 		return;
 
+
 		void initValues()
 		{
 			this.ParentZone	= this.GetComponentInParent<ZoneBase>();
+			this.rndrr		= this.GetComponent<Renderer>();
 			this.mpb		= new MaterialPropertyBlock();
-			this.GetComponent<MeshRenderer>().SetPropertyBlock( this.mpb );
+			this.backLinks	= new List<LinkUnit>();
 		}
 	}
 
 
-	void Start()
+	protected void Start()
     {
 		
 		initLocation();
@@ -75,47 +96,75 @@ public class NeuronUnit : MonoBehaviour
 		return;
 
 
+		void initLocation()
+		{
+			this.position		= this.transform.position;
+			this.linkerCenter	= this.ParentZone.CalucNeuronForwardosition( this.position, this.ParentZone.UnitLinkArmDistance );
+		}
+
 		void setLinks()
 		{
-			this.forwardLinks = Physics.OverlapSphere( this.linkerCenter, this.UnitLinkRadius )
+			this.forwardLinks = Physics.OverlapSphere( this.linkerCenter, this.ParentZone.UnitLinkRadius )
 				.Select( col => col.GetComponent<NeuronUnit>() )
 				.Where( n => n != null )
 				.Where( n => n != this )
 				.Where( n => this.ParentZone.IsLinkTarget(n.ParentZone) )
-				.Select( n => (n, 0.0f) )
-				.Select( x => (x.n, Random.value))// v のランダム初期化
+				.Select( n => new LinkUnit{ start = this, end = n, value = Random.value } )
 				.ToArray()
 				;
+			foreach( var link in this.forwardLinks )
+			{
+				link.end.backLinks.Add( link );
+			}
 		}
     }
 
 
-	private void Update()
+	protected void Update()
 	{
 		showState();
 		showLink();
-	}
-
-
-
-	void initLocation()
-	{
-        this.position		= this.transform.position;
-		this.linkerCenter	= this.ParentZone.CalucNeuronForwardosition( this.position, this.UnitLinkArmDistance );
+		//this.volume = 0;
 	}
 
 	
 	void showState()
 	{
-		var vol = this.volume;
-		this.mpb.SetColor( NeuronUnit.colorNameId, new Color(vol,vol,vol) );
+		this.mpb.SetColor( NeuronUnit.colorNameId, this.volume.ToColor() );
+		this.rndrr.SetPropertyBlock( this.mpb );
 	}
+
 	void showLink()
 	{
-		Debug.DrawLine( this.position, this.linkerCenter );
+		Debug.DrawLine( this.position, this.linkerCenter, this.volume.ToColor() );
 		foreach( var link in forwardLinks )
 		{
-			Debug.DrawLine( this.linkerCenter, link.n.position );
+			Debug.DrawLine( this.linkerCenter, link.end.position, link.value.ToColor() );
 		}
+	}
+
+	
+}
+
+
+public class LinkUnit
+{
+	public NeuronUnit	start;
+	public NeuronUnit	end;
+
+	public float		value;
+}
+
+
+static class ColorExtentions
+{
+	static public Color ToColor( ref this Vector3 v )
+	{
+		return new Color( v.x, v.y, v.z );
+	}
+	static public Color ToColor( this float f )
+	{
+		var v = Vector3.one * f;
+		return v.ToColor();
 	}
 }
---
+++ Assets/NeuronUnitTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeuronUnitTrigger : NeuronUnit
+{
+
+
+	new void Awake() => base.Awake();
+	new void Start() => base.Start();
+
+	new void Update()
+	{
+
+		this.Clear();
+
+		this.Emit( 1.0f );
+
+
+		base.Update();
+	}
+
+}
