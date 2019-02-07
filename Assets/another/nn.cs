631c685a5b66107bde4be43e6c6a29769a514d8d
abarabone <abara00@live.jp>
2019-02-07 23:36:18 +0900

    a

--- Assets/another/nn.cs
+++ Assets/another/nn.cs
@@ -9,8 +9,8 @@ namespace a
 
 	public class NeuronUnit
 	{
-		public List<LinkUnit>	forwards;
-		public List<LinkUnit>	backs;
+		public LinkUnit[]	forwards;
+		public LinkUnit[]	backs;
 
 		public float	bias;
 		public float	activation;
@@ -39,13 +39,13 @@ namespace a
 			var modify_backs = this.backs
 				.Select( link => this.delta_value * link.back.activation * this.learning_rate )
 				;
-			foreach( var (modify, link) in Enumerable.Zip(modify_backs, this.backs) )
+			foreach( var (modify, link) in Enumerable.Zip(modify_backs, this.backs, (x,y) => (x,y) ) )
 			{
 				link.weight += modify;
+				this.bias	+= modify;
 			}
-			this.bias += modify_backs.Sum();
 		}
-		public void init()
+		public NeuronUnit()
 		{
 			this.f = sum_value => 1.0f / ( 1.0f + (float)Math.Exp((float)-sum_value) );
 			this.d = activation => activation * ( 1.0f - activation );
@@ -63,25 +63,66 @@ namespace a
 
 	public class LayerUnit
 	{
-		public List<NeuronUnit>	neurons;
+		public NeuronUnit[]	neurons;
+		public LayerUnit( int length ) => this.neurons = new NeuronUnit[ length ];
 	}
 
 	public class N
 	{
-		public List<LayerUnit>	layers;
+		public LayerUnit[]	layers =
+		{
+			new LayerUnit( 2 ),
+			new LayerUnit( 1 )
+		};
+
+		public N()
+		{
+			init_links();
+		}
 
 		public void propergate_forward()
 		{
-			foreach( var n in layers.SelectMany( layer => layer.neurons ) )
+			//foreach( var n in layers.SelectMany( layer => layer.neurons ) )
+			foreach( var n in from layer in this.layers from n in layer.neurons select n )
 			{
 				n.activate();
 			}
 		}
 		public void propergate_back()
 		{
-			foreach( var n in layers.Reverse<LayerUnit>().SelectMany( layer => layer.neurons ) )
+			//foreach( var n in layers.Reverse<LayerUnit>().SelectMany( layer => layer.neurons ) )
+			foreach( var n in from layer in this.layers.Reverse<LayerUnit>() from n in layer.neurons select n )
 			{
 				n.learn();
+			}
+		}
+
+		private void init_links()
+		{
+			this.layers.Aggregate( (prev_layer, next_layer) => set_links_to_both_side_nodes_( prev_layer, next_layer ) );
+
+			return;
+
+			LayerUnit set_links_to_both_side_nodes_( LayerUnit prev_layer, LayerUnit next_layer )
+			{
+				var q = from pn in prev_layer.neurons
+						from nn in next_layer.neurons
+						select new LinkUnit
+						{
+							back	= pn,
+							forward	= nn,
+							weight	= 0,
+						}
+						;
+				foreach( var prev_group in from n in q group q by n.back )
+				{
+					prev_group.Key.forwards = prev_group.SelectMany( links => links ).ToArray();
+				}
+				foreach( var next_group in from n in q group q by n.forward )
+				{
+					next_group.Key.forwards = next_group.SelectMany( links => links ).ToArray();
+				}
+				return next_layer;
 			}
 		}
 	}
